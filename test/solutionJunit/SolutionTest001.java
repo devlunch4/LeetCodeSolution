36267e29d8e0d9ac919d01460ce38eed46616fe9
devlunch4 <devlunch4@gmail.com>
2022-06-10 11:51:46 +0900

    Added: Junit Test001 HashMap Solution Test.

--- test/solutionJunit/SolutionTest001.java
+++ test/solutionJunit/SolutionTest001.java
@@ -1,22 +1,22 @@
 package solutionJunit;
 
 import org.junit.jupiter.api.Test;
+import problem001TwoSum.Solution;
+import problem001TwoSum.SolutionHashMap;
 
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
-import static problem001TwoSum.Solution.twoSum;
 
 class SolutionTest001 {
-
     @Test
     public void test01() {
         // Example 1:
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] ourput = {0, 1};
-        String answer = Arrays.toString(twoSum(nums, target));
+        String answer = Arrays.toString(Solution.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
@@ -29,7 +29,7 @@ class SolutionTest001 {
         int[] nums = {3, 2, 4};
         int target = 6;
         int[] ourput = {1, 2};
-        String answer = Arrays.toString(twoSum(nums, target));
+        String answer = Arrays.toString(Solution.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
@@ -38,11 +38,50 @@ class SolutionTest001 {
 
     @Test
     public void test03() {
-        // Example 2:
+        // Example 3:
         int[] nums = {3, 3};
         int target = 6;
         int[] ourput = {0, 1};
-        String answer = Arrays.toString(twoSum(nums, target));
+        String answer = Arrays.toString(Solution.twoSum(nums, target));
+        String methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        System.out.println(methodName + " answer Value:" + answer);
+        assertEquals(Arrays.toString(ourput), answer);
+    }
+
+    @Test
+    public void test04() {
+        // Example 1:
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] ourput = {0, 1};
+        String answer = Arrays.toString(SolutionHashMap.twoSum(nums, target));
+        String methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        System.out.println(methodName + " answer Value:" + answer);
+        assertEquals(Arrays.toString(ourput), answer);
+    }
+
+    @Test
+    public void test05() {
+        // Example 2:
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] ourput = {1, 2};
+        String answer = Arrays.toString(SolutionHashMap.twoSum(nums, target));
+        String methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        System.out.println(methodName + " answer Value:" + answer);
+        assertEquals(Arrays.toString(ourput), answer);
+    }
+
+    @Test
+    public void test06() {
+        // Example 3:
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] ourput = {0, 1};
+        String answer = Arrays.toString(SolutionHashMap.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
