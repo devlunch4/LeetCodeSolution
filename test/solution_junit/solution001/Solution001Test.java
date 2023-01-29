bd014cfbcb92bb43520ff0f9df82ca0ed5eeba1b
devlunch In MacOs <devlunch4@gmail.com>
2023-01-29 12:45:23 +0900

    code line modified.

--- test/solution_junit/solution001/Solution001Test.java
+++ test/solution_junit/solution001/Solution001Test.java
@@ -1,21 +1,21 @@
 package solution_junit.solution001;
 
-import org.junit.jupiter.api.Assertions; //JUnit5
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import problem001_two_sum.Solution;
-import problem001_two_sum.SolutionHashMap;
+import solution.Solution001TwoSum;
+import solution.Solution001TwoSumHashMap;
 
 import java.util.Arrays;
 // import static org.junit.Assert.assertEquals; //JUnit4
 
-class SolutionTest {
+class Solution001Test {
     @Test
     public void test01() {
         // Example 1:
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] ourput = {0, 1};
-        String answer = Arrays.toString(Solution.twoSum(nums, target));
+        String answer = Arrays.toString(Solution001TwoSum.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
@@ -28,7 +28,7 @@ class SolutionTest {
         int[] nums = {3, 2, 4};
         int target = 6;
         int[] ourput = {1, 2};
-        String answer = Arrays.toString(Solution.twoSum(nums, target));
+        String answer = Arrays.toString(Solution001TwoSum.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
@@ -41,7 +41,7 @@ class SolutionTest {
         int[] nums = {3, 3};
         int target = 6;
         int[] ourput = {0, 1};
-        String answer = Arrays.toString(Solution.twoSum(nums, target));
+        String answer = Arrays.toString(Solution001TwoSum.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
@@ -54,7 +54,7 @@ class SolutionTest {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] ourput = {0, 1};
-        String answer = Arrays.toString(SolutionHashMap.twoSum(nums, target));
+        String answer = Arrays.toString(Solution001TwoSumHashMap.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
@@ -67,7 +67,7 @@ class SolutionTest {
         int[] nums = {3, 2, 4};
         int target = 6;
         int[] ourput = {1, 2};
-        String answer = Arrays.toString(SolutionHashMap.twoSum(nums, target));
+        String answer = Arrays.toString(Solution001TwoSumHashMap.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
@@ -80,7 +80,7 @@ class SolutionTest {
         int[] nums = {3, 3};
         int target = 6;
         int[] ourput = {0, 1};
-        String answer = Arrays.toString(SolutionHashMap.twoSum(nums, target));
+        String answer = Arrays.toString(Solution001TwoSumHashMap.twoSum(nums, target));
         String methodName = new Object() {
         }.getClass().getEnclosingMethod().getName();
         System.out.println(methodName + " answer Value:" + answer);
